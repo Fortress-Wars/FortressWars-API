55d75759f1883e726eb86d584662a2c9cbb5b946
Peter Cesmegi <petercesmegi@gmail.com>
2026-05-26 19:31:12 -0400

    Remove cosmetics from api

--- src/main/java/net/fortresswars/core/cosmetics/CosmeticType.java
+++
@@ -1,9 +0,0 @@
-package net.fortresswars.core.cosmetics;
-
-public enum CosmeticType {
-    DEATH_ANIMATION,
-    PARTICLE_EFFECT,
-    ARROW_TRAILS,
-    BLOCK_BREAK_EFFECT,
-    BLOCK_PLACE_EFFECT
-}
