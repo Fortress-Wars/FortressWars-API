4e073e6ace23a1c54818fe40960b1dcff1efb297
Peter Cesmegi <petercesmegi@gmail.com>
2026-09-05 16:19:54 -0400

    Make fw-api as a plugin so it can be provided

--- src/main/java/net/fortresswars/FortressWarsAPI.java
+++ src/main/java/net/fortresswars/FortressWarsAPI.java
@@ -1,6 +1,8 @@
 package net.fortresswars;
 
-public final class FortressWarsAPI {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FortressWarsAPI extends JavaPlugin {
 
     public final static String NAMESPACE = "fwapi";
 }
