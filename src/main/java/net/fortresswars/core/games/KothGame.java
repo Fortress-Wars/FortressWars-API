dc26c346af3c53f20ea456c70b682b32e809db17
Peter Cesmegi <petercesmegi@gmail.com>
2026-09-16 07:52:01 -0400

    Add KothGame interface

---
+++ src/main/java/net/fortresswars/core/games/KothGame.java
@@ -0,0 +1,8 @@
+package net.fortresswars.core.games;
+
+import net.fortresswars.core.player.TeamColor;
+
+public interface KothGame extends BeaconGame {
+
+    TeamColor getTeamControllingBeacon();
+}
